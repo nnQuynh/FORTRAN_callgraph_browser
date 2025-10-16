bb35698cb65bf638212c921fde752f0ab688793b
nnQuynh <33197640+nnQuynh@users.noreply.github.com>
2025-10-16 18:02:03 +0700

    m

--- allfortran.py
+++ allfortran.py
@@ -49,8 +49,9 @@ print(args)
 # get a list of all paths
 # __loc__ = 'flexpart - Copy' # args.path
 # __loc__ = 'phits_small' # args.path
-__loc__ = 'phits' # args.path
+# __loc__ = 'phits' # args.path
 # __loc__ = 'angel' # args.path
+__loc__ = 'flexpart' # args.path
 allpaths = list(Path(__loc__).rglob('*.F*'))
 allpaths.extend(list(Path(__loc__).rglob('*.f*')))
 
